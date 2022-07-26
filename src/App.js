8223a16d9cba6df4c7554d586a0c40081f496030
kalhollywood <kal.hollywood@gmail.com>
2022-07-26 12:24:04 +0100

    turnup commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hey guys</h1>
+        <img src="../turnuplogo.png" className="App-logo" alt="logo" />
+        <h1>TURN UP</h1>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
