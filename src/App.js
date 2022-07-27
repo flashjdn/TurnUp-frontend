c7a3a1b86f033391f4f3bd5cd4b3c23c74c40e95
Jordan Flash <flash.jordan.3@gmail.com>
2022-07-27 16:49:58 +0100

    refactoring of the accordian begiun. we now have a basic card that feeds in the event title, organiser and date from some dummy data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
-import LandingPage from "./Components/Home";
+import Card from "./Components/Card";
+// import LandingPage from "./Components/Home";
+import { eventObject } from "./lib/dummy";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LandingPage />
+        {/* <LandingPage /> */}
+        <Card
+          eventName={eventObject.eventName}
+          eventDate={eventObject.eventDate}
+          eventOrganiser={eventObject.eventOrganiser}
+        
+          
+        />
       </header>
     </div>
   );
