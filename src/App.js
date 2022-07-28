2054cce62c00ce930468e2eafd9a25835579d46b
OwenB-HamD <owen.b@hamiltoniandynamics.com>
2022-07-28 08:53:23 +0100

    added video to main branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
-import Card from "./Components/Card";
-// import LandingPage from "./Components/Home";
-import { eventObject } from "./lib/dummy";
+// import Card from "./Components/Card";
+import LandingPage from "./Components/Home";
+// import { eventObject } from "./lib/dummy";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <LandingPage /> */}
-        <Card
+        <LandingPage />
+        {/* <Card
           eventName={eventObject.eventName}
           eventDate={eventObject.eventDate}
           eventOrganiser={eventObject.eventOrganiser}
         
           
-        />
+        /> */}
       </header>
     </div>
   );
