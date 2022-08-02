c5ece06f60e3588a1cf022e5c6ef686d99b32a5f
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-08-02 17:10:50 +0100

    friends list made lightweight

--- src/Components/FriendsCard/index.js
+++ src/Components/FriendsCard/index.js
@@ -8,7 +8,7 @@ export const FriendsCard = () => {
                 {" "}
                 <div className="friend-name-box">
                 </div>
-                <div className="card-one-text">
+                <div className="friend-card-one-text">
 
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5GNLQ5Rq4_uCHZY7yxKiYXxjkkhro_aIbGQ&usqp=CAU" className="friend-pic" alt="friend-img">
                     </img> <p className="friend-name"> Friend Name</p>
