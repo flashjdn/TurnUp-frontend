14ea946df36d4c818fa176bd25d8aad2b2815c17
Jordan Flash <flash.jordan.3@gmail.com>
2022-08-04 02:39:11 +0100

    drawer component anchored to bottom, made it pretty with css and more accessible to mobile

--- src/Components/Drawer/index.js
+++ src/Components/Drawer/index.js
@@ -1,6 +1,6 @@
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import { useState } from "react";
-
+import "./styles.css";
 
 export default function Drawer() {
   const [drawerState, setDrawerState] = useState(false);
@@ -13,23 +13,37 @@ export default function Drawer() {
     setDrawerState(false);
   }
 
-// const theme = createTheme()
-
-//   const useStyles = makeStyles({
-//     SwipeableDrawer: {
-//       backgroundColor: "#fadedd",
-//     },
-//   });
-
   return (
-    <div>
+    <div className="drawer-wrapper">
       <SwipeableDrawer
         open={drawerState}
         onOpen={handleOpen}
         onClose={handleClose}
-        anchor="left"
+        anchor="bottom"
+        PaperProps={{
+          sx: {
+            maxWidth: "100%",
+            backgroundColor: "var(--secondary)",
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "10px 10px 0px 10px",
+            padding: "5px 15px 5px 15px",
+            textAlign: "justify",
+            borderRadius: "10px",
+          },
+        }}
       >
-        <h1>Hey this totally works</h1>
+        <h1>Turn up, for what? 🤨</h1>
+        <p>
+          Et ipsum tempor ullamco voluptate sunt. Ut id eu occaecat officia
+          aliqua excepteur laboris do velit. Veniam minim pariatur qui ex.
+          Labore voluptate Lorem fugiat nisi nostrud officia anim nostrud
+          voluptate proident nulla in aute. Exercitation incididunt consequat
+          occaecat aliquip ipsum fugiat ex do. Ut commodo non quis aute aliqua
+          mollit in do nisi nulla.
+        </p>
       </SwipeableDrawer>
       <button className="about-text" onClick={handleOpen}>
         What is this?
