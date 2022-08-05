815982197c2159311cdc4006fc2974e660704644
Jordan Flash <flash.jordan.3@gmail.com>
2022-08-05 16:02:54 +0100

    working db link

--- src/Models/getEvents.js
+++ src/Models/getEvents.js
@@ -1,5 +1,5 @@
 export const getEvents = async () => {
-  const res = await fetch(``);
+  const res = await fetch(`https://turnupdb.herokuapp.com/events/all`);
   const data = await res.json();
   // console.log(data);
   return data.payload;
