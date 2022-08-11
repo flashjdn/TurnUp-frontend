1466861e8c872bd5d59f8e70d1cfe4567515b874
Jordan Flash <flash.jordan.3@gmail.com>
2022-08-11 01:20:24 +0100

    reconfigured the css and responsiveness of the create new event page, it still needs some work, and also needs the address search bar added

--- src/Components/AddEventCard/index.js
+++ src/Components/AddEventCard/index.js
@@ -13,13 +13,12 @@ import {
   FormGroup,
 } from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
-import { StaticTimePicker } from "@mui/x-date-pickers/StaticTimePicker";
-
+import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { useNavigate } from "react-router-dom";
 import { DocumentScanner } from "@mui/icons-material";
 
 import Places from "../Places/places";
-
 
 export default function NewEventForm({ onClick }) {
   //Form submission function that reads each input type and adds it to the object to be sent to the server if needed.
@@ -33,10 +32,10 @@ export default function NewEventForm({ onClick }) {
   const [time, setTime] = useState("");
   const [coord, setCoord] = useState({ lat: 0, lng: 0 });
 
-  console.log("these are the coords", coord);
+  // console.log("these are the coords", coord);
 
-
-  console.log(tags);
+  const navigate = useNavigate();
+  // console.log(tags);
   const handleTagChange = (e) => {
     const index = tags.indexOf(e.target.value);
     if (index === -1) {
@@ -45,7 +44,6 @@ export default function NewEventForm({ onClick }) {
       setTags(tags.filter((tag) => tag !== e.target.value));
     }
   };
-
 
   function handleName(event) {
     // This function tracks the string information typed into the input field.
@@ -68,10 +66,9 @@ export default function NewEventForm({ onClick }) {
   function handleDate(event) {
     // This function tracks the string information typed into the input field.
     const value = event.target.value;
-    const newVal = Date().toLocaleString(value)
+    const newVal = Date().toLocaleString(value);
     setDate(newVal);
   }
-
 
   function handleTime(event) {
     // This function tracks the string information typed into the input field.
@@ -104,7 +101,7 @@ export default function NewEventForm({ onClick }) {
     // ADAPT ALL OF THE BELOW TO MATCH OUR DATA
     document.querySelector(".event-form-container").classList.add("hidden");
 
-    console.log(eventObj);
+    // console.log(eventObj);
 
     //    /Grabs the 6 current tags to idenitfy checked status. happy to help checkbox is also included but
     //is ignored as is handled later.
@@ -120,7 +117,7 @@ export default function NewEventForm({ onClick }) {
 
     //  All elements have been searched, ready to post the data to the server and database.
     const response = await fetch(`https://turnupdb.herokuapp.com/events/all/`, {
-      // 
+      //
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -141,204 +138,174 @@ export default function NewEventForm({ onClick }) {
     return response.json();
   }
 
-  // if (document.getElementById("resources-input").value !== "") {
-  //   await fetch(`http://localhost:3001/resource`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(newResourceObj),
-  //   });
-  // }
-  // if (document.getElementById("happy-to-help-input").checked) {
-  //   await fetch(`http://localhost:3001/help?email=${user.email}`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(newResourceObj),
-  //   });
-  // }
-  //  Resets form and then reloads page
-  // document.querySelector("#notes-input-field").reset();
-  // window.location.reload();
-  // }
   function hideForm() {
     document.querySelector(".event-form-container").classList.add("hidden");
   }
 
   function test() {
     console.log(eventObj);
-    console.log(document.getElementsByClassName("event-title-box").value);
   }
 
   return (
     <div>
       <Navbar></Navbar>
 
-      <section className="event-form-container">
-        <form
-          id="event-input-field"
-          onSubmit={(e) => {
-            handleSubmission(e);
-          }}
-        >
-          <div className="create-event-card-container">
-            <div className="title-sum-desc-container">
-              <TextField
+      <div className="form-container">
+        <div className="top-left">
+          <TextField
+            onChange={handleName}
+            className="event-title-box"
+            sx={{
+              width: "35vw",
+              height: "5rem",
+              // background: "var(--supporting-blue)",
+              margin: "auto",
+              display: "inline-flex",
+            }}
+            label="Event title"
+            multiline
+            rows={2}
+            cols={100}
+            defaultValue=""
+            id="titleArea"
+            required
+            inputProps={{ maxLength: "40" }}
+          />
 
-                onChange={handleName}
+          <TextField
+            onChange={handleSummary}
+            className="event-summary-box"
+            sx={{
+              width: "35vw",
+              height: "9.2rem",
+              // background: "var(--supporting-blue)",
+            }}
+            label="Event Summary"
+            multiline
+            rows={5}
+            cols={100}
+            defaultValue=""
+            id="summaryArea"
+            required
+            inputProps={{ maxLength: "80" }}
+          />
 
-                className="event-title-box"
-                sx={{
-                  width: "40rem",
-                  height: "5rem",
-                  background: "var(--supporting-blue)",
+          <TextField
+            onChange={handleDescription}
+            className="event-description-box"
+            sx={{
+              width: "35vw",
+              height: "15rem",
+              // background: "var(--supporting-blue)",
+            }}
+            label="Event Description"
+            multiline
+            rows={6}
+            cols={100}
+            defaultValue=""
+            id="decriptionArea"
+            required
+          />
+          {/* <div className="buffer"> </div> */}
+        </div>
+        <div className="top-right">
+          <div className="date-time-container">
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Event date"
+                orientation="landscape"
+                openTo="day"
+                value={date}
+                inputFormat="dd.MM.yyyy"
+                onChange={(newDate) => {
+                  setDate(newDate);
                 }}
-                label="Event title:"
-                multiline
-                rows={2}
-                cols={100}
-                defaultValue=""
-                id="titleArea"
-                required
-                inputProps={{ maxLength: "40" }}
+                renderInput={(params) => <TextField {...params} />}
+                // sx={{
+                //   width: "auto",
+                //   height: "auto",
+                // }}
               />
+            </LocalizationProvider>
 
-              <TextField
-
-                onChange={handleSummary}
-
-                className="event-summary-box"
-                sx={{
-                  width: "40rem",
-                  height: "9.2rem",
-                  background: "var(--supporting-blue)",
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <TimePicker
+                // ampmInClock="false"
+                label="Start time"
+                orientation="landscape"
+                openTo="hours"
+                inputFormat="hh:mm"
+                value={time}
+                onChange={(newTime) => {
+                  setTime(newTime);
                 }}
-                label="Event Summary:"
-                multiline
-                rows={5}
-                cols={100}
-                defaultValue=""
-                id="summaryArea"
-                required
-                inputProps={{ maxLength: "80" }}
+                renderInput={(params) => <TextField {...params} />}
+                // sx={{
+                //   width: "auto",
+                //   height: "auto",
+                // }}
               />
-
-              <TextField
-
-                onChange={handleDescription}
-
-                className="event-description-box"
-                sx={{
-                  width: "40rem",
-                  height: "15rem",
-                  background: "var(--supporting-blue)",
-                }}
-                label="Event Description:"
-                multiline
-                rows={9}
-                cols={100}
-                defaultValue=""
-                id="decriptionArea"
-                required
-              />
-
-              <Box>
-                <Box className="tag-area-box">
-                  <FormControl>
-                    <FormLabel>Event Tags</FormLabel>
-                    <FormGroup row>
-                      <FormControlLabel
-                        label="Pet-Friendly"
-                        control={<Checkbox value="pet-friendly" />}
-                        onChange={handleTagChange}
-                      />
-                      <FormControlLabel
-                        label="18+"
-                        control={<Checkbox value="18+" />}
-                        onChange={handleTagChange}
-                      />
-                      <FormControlLabel
-                        label="Outdoors"
-                        control={<Checkbox value="outdoors" />}
-                        onChange={handleTagChange}
-                      />
-                      <FormControlLabel
-                        label="Parking"
-                        control={<Checkbox value="parking" />}
-                        onChange={handleTagChange}
-                      />
-                      <FormControlLabel
-                        label="Family-Friendly"
-                        control={<Checkbox value="family-friendly" />}
-                        onChange={handleTagChange}
-                      />
-                    </FormGroup>
-                  </FormControl>
-                </Box>
-              </Box>
-            </div>
-
-            <div className="date-time-container">
-              <div className="date">
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <StaticDatePicker
-                    orientation="landscape"
-                    openTo="day"
-                    value={date}
-                    inputFormat="dd.MM.yyyy"
-                    onChange={(newDate) => {
-                      setDate(newDate);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-              </div>
-
-              <div className="time">
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <StaticTimePicker
-                    // ampmInClock="false"
-                    orientation="landscape"
-                    openTo="hours"
-                    inputFormat="hh:mm"
-                    value={time}
-                    onChange={(newTime) => {
-                      setTime(newTime);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-              </div>
-            </div>
+            </LocalizationProvider>
           </div>
-
-          <Button
-            sx={{ top: "3rem", left: "3rem" }}
-            variant="contained"
-            type="submit"
-            onClick={test}
-          >
-            Submit
-          </Button>
-        </form>
-        <a href="/profile">
-          <Button
-            sx={{ bottom: "26rem", right: "4.5vh" }}
-            id="exit-button-form"
-            variant="contained"
-            aria-label="Cancel and hide current event submission form"
-            onClick={onClick}
-          >
-            X
-          </Button>
-        </a>
-      </section>
-
-      <Places setCoordFunction={setCoord} />
-
+          <div className="tag-area-box">
+            <FormControl>
+              <FormLabel>Event Tags</FormLabel>
+              <FormGroup row>
+                <FormControlLabel
+                  label="Pet-Friendly"
+                  control={<Checkbox value="pet-friendly" />}
+                  onChange={handleTagChange}
+                />
+                <FormControlLabel
+                  label="18+"
+                  control={<Checkbox value="18+" />}
+                  onChange={handleTagChange}
+                />
+                <FormControlLabel
+                  label="Outdoors"
+                  control={<Checkbox value="outdoors" />}
+                  onChange={handleTagChange}
+                />
+                <FormControlLabel
+                  label="Parking"
+                  control={<Checkbox value="parking" />}
+                  onChange={handleTagChange}
+                />
+                <FormControlLabel
+                  label="Family-Friendly"
+                  control={<Checkbox value="family-friendly" />}
+                  onChange={handleTagChange}
+                />
+              </FormGroup>
+            </FormControl>
+          </div>
+          <div className="buttons">
+            <Button
+              sx={{
+                margin: "auto",
+              }}
+              variant="contained"
+              size="large"
+              color="error"
+              type="submit"
+              onClick={() => navigate("/profile")}
+            >
+              Cancel
+            </Button>
+            <Button
+              sx={{
+                margin: "auto",
+              }}
+              variant="contained"
+              type="submit"
+              size="large"
+              onClick={handleSubmission}
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
