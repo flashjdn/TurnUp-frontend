d85a19c5464c81b79b39ba087e9be8efcb06bd88
OwenB-HamD <owen.b@hamiltoniandynamics.com>
2022-08-11 14:02:43 +0100

    create event card title done, attempting font effects

---
+++ src/Components/CreateEventTitle/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function createEventTitle() {
+    return (
+        <div className="create-event-title-title">
+            <h1 className="create-title">Create your Event</h1>
+        </div>
+    )
+}
