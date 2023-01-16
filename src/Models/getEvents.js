b3af221ab8105020872cbcbebb8da73ef7ea5581
Jordan Flash <flash.jordan.3@gmail.com>
2023-01-16 17:15:55 +0000

    new fork, connected to flashjdn personal heroku backend and updated endpoints. cleared some irrelevant files.

--- src/Models/getEvents.js
+++ src/Models/getEvents.js
@@ -1,5 +1,5 @@
 export const getEvents = async () => {
-  const res = await fetch(`https://turnupdb.herokuapp.com/events/all`);
+  const res = await fetch(`https://new-turnup.herokuapp.com/events/all`);
   const data = await res.json();
   console.log(data);
   return data.payload;
